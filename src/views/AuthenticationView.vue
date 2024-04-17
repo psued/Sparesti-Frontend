4e6e8bd0e3c68b3a05d55919c46a0e1b9bccb018
Zahid André Kristiansen <zahidak@stud.ntnu.no>
2024-04-17 09:20:38 +0200

    style: :art: Fixed grid for login
    Fixed the way grid dynamically behaves for Login, Signup, and completeaccount form
    
    main.css removed code that tries to dynamically fix the grid placements

--- src/views/AuthenticationView.vue
+++ src/views/AuthenticationView.vue
@@ -1,10 +1,10 @@
 <template>
-	<div>
+	<div class="authentication-container">
 		<div class="static-content">
 			<p>Welcome to SpareSti! <br>Application to make your wallet happier. <br>Free to use, money back guarantee.</p>
 		</div>
 		<div class="dynamic-content">
-			<router-view class="dynamic-content"></router-view>
+			<router-view></router-view>
 		</div>
 	</div>
 </template>
@@ -13,10 +13,40 @@
 </script>
 
 <style scoped>
+
+
+.authentication-container {
+  display: grid;
+  grid-template-columns: 2fr 1fr; 
+  align-items: center; 
+  justify-content: center; 
+  min-height: 100vh; 
+  background-color: aqua;
+	width: 100%;
+}
+
+.dynamic-content {
+}
+
 .static-content {
 	font-size: 1.5rem;
-	text-align: center;
+	text-align: left;
 	margin-top: 2rem;
 	color: #000;
+}
+
+@media (max-width: 768px) {
+  .authentication-container {
+    grid-template-columns: 1fr; /* Makes it a single column layout */
+  }
+
+  .static-content {
+    display: none; /* Hides the static content */
+  }
+
+  .dynamic-content {
+    display: flex;
+    justify-content: center;
+  }
 }
 </style>
