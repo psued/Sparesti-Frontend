6cec81ae253d5e10058fd7e4a50f3aca3a1b7312
anderebe <anderebe@stud.ntnu.no>
2024-04-18 15:14:21 +0200

    feat: :art: Fixed the nav to be fully functional and mobile friendly
    The nav now works as intended and is fullt funtional both for desktop and mobile. There has also been implemented a dark/light mode with a button. The buttons are also visible with a focus hover on all the buttons and symbols

--- src/components/nav/Sidebar.vue
+++ src/components/nav/Sidebar.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="sidebar" ref="sidebarRef" :class="{ 'sidebar-open': isOpen }">
-        <div class="line" />
-        <div class="top-bar-items">
-            <div class="sign-out" >
-                <i class="icon-sign-out" />
+    <div class="sidebar" ref="sidebarRef" :class="{ 'sidebar-open': isOpen, 'sidebar-dark': darkMode}">
+        <div class="line" :class="{'line-dark' : darkMode}" />
+        <div class="top-bar-items" >
+            <div class="sign-out"  :class="{'sign-out-dark': darkMode}" @click="toggleBar" @mouseenter="toggleHoverSign" @mouseleave="toggleHoverSign" >
+                <i :class="darkMode ?  'icon-sign-out-dark' : (!hoverSign ? 'icon-sign-out' : 'icon-sign-out-dark')"  />
             </div>
-                <div class="theme-mode" @click="toggleTheme">
-                    <i :class="iconTheme ? 'icon-light-mode' : 'icon-dark-mode'" />
-                </div>
+            <div class="theme-mode" :class="{'theme-mode-dark': darkMode}" @click="toggleTheme" @mouseenter="toggleHoverTheme" @mouseleave="toggleHoverTheme">
+                <i :class="[darkMode ? 'icon-light-mode' : (!hoverTheme ? 'icon-dark-mode' : 'icon-dark-mode-hover')]" />
             </div>
+        </div>
         <div class="bar-items">
-            <router-link :class="isPhone ? 'bar-item-phone phone-button' : 'bar-item-desktop'" to="/">
+            <router-link v-if=(!isPhone) class="bar-item-desktop" :class="[{'text-dark' : darkMode}]" @click="toggleBar" to="/">
                 <text>Home</text>
             </router-link>
-            <router-link class="bar-item-desktop" to="/profile">
+            <router-link class="bar-item-desktop" :class="{'text-dark' : darkMode}" @click="toggleBar" to="/profile">
                 <text>Profile</text>
             </router-link>
-            <router-link :class="isPhone ? 'bar-item-phone phone-button' : 'bar-item-desktop'" to="/budget">
+            <router-link v-if=(!isPhone) class="bar-item-desktop" :class="[{'text-dark' : darkMode}]" @click="toggleBar"  to="/budget">
                 <text>Budget</text>
             </router-link>
-            <router-link :class="isPhone ? 'bar-item-phone phone-button' : 'bar-item-desktop'" to="/challenges">
+            <router-link v-if=(!isPhone) class="bar-item-desktop" :class="[{'text-dark' : darkMode}]" @click="toggleBar" to="/challenges">
                 <text>Challenges</text>
             </router-link>
-            <router-link class="bar-item-desktop" :class="isPhone ? '' : 'contact'"  to="/Contact">
+            <router-link class="bar-item-desktop" :class="[isPhone ? '' : 'contact', {'text-dark' : darkMode}]" @click="toggleBar"  to="/contact">
                 <text>Contact</text>
             </router-link>
         </div>
@@ -30,20 +30,38 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, defineEmits} from 'vue';
+import { onMounted, ref, defineEmits, defineProps} from 'vue';
 const isOpen = ref(false);
 const isPhone = ref(false);
-const iconTheme = ref(false);
 const emit = defineEmits();
+const hoverSign = ref(false);
+const hoverTheme = ref(false);
 
 const getToken = () => {
     return localStorage.getItem('token');
 }
 
 const toggleTheme = () => {
-    iconTheme.value = !iconTheme.value;
     emit('theme');
 }
+const toggleBar = () => {
+    emit('bar');
+}
+
+const toggleHoverSign = () => {
+    hoverSign.value = !hoverSign.value;
+}
+
+const toggleHoverTheme = () => {
+    hoverTheme.value = !hoverTheme.value;
+}
+
+const props = withDefaults(defineProps<{
+  darkMode: boolean
+}>(), {
+  darkMode: false
+});
+
 onMounted(() => {
     const mediaQuery = window.matchMedia('(max-width: 480px)');
     const handleMediaChange = (e: any) => {
@@ -60,21 +78,26 @@ onMounted(() => {
     z-index: 999;
     top: 0;
     right: 0;
-    background-color: #a6cd94;
+    background-color: #a6cd94 ;
     height: 100vh;
     width: 100vw;
+    border-left: 2px solid #4b644a;
+}
+.sidebar-dark {
+    border-left: 2px solid #757bfd;
+    background-color: #23244b;
 }
 
 .top-bar-items{
     position: absolute;
     top: 0;
-    margin-top: 15px;
     left: 0;
     display: flex;
     flex-direction: row;
     align-items: center;
     height: 90px;
     width: 100%;
+    overflow: hidden;
 }
 
 .sign-out{
@@ -88,6 +111,10 @@ onMounted(() => {
     border-right: 2px solid #4b644a;
     transition: 0.5s; 
 }
+.sign-out-dark{
+    border-right: 2px solid #757bfd;
+}
+
 .sign-out:hover {
     background-color: rgba(255, 0, 0, 0.8);
     color: #f0f0f0;
@@ -104,17 +131,33 @@ onMounted(() => {
     border-right: 2px solid #4b644a;
     transition: 0.5s; 
 }
+
+.theme-mode-dark{
+    border-right: 2px solid #757bfd;
+
+}
+
 .theme-mode:hover {
     background-color: #4b644a;
+    .icon-dark-mode{
+        stroke: #f0f0f0;
+    }
     color: #f0f0f0;
+}
+
+.theme-mode-dark:hover{
+    background: #757bfd;
 }
 
 
 .line {
-    margin-top: 120px;
+    margin-top: 89px;
     width: 100%;
     height: 2px;
     background-color: #4b644a;
+}
+.line-dark {
+    background-color: #757bfd;
 }
 
 .bar-items {
@@ -136,6 +179,9 @@ onMounted(() => {
     transform: 0.5s;
     text-decoration: none;
 }
+.text-dark{
+    color: white;
+}
 
 .bar-item-desktop.contact{
     position: absolute;
@@ -147,9 +193,14 @@ onMounted(() => {
     color: #f0f0f0;
 }
 
+.bar-item-desktop.text-dark:hover {
+    background-color: #757bfd;
+    color: #f0f0f0;
+}
+
 @media screen and (min-width: 481px){
     .sidebar {
-        width: 50vw;
+        width: 40vw;
         min-width: 350px;
     }
 }
--- src/views/Nav_View.vue
+++ src/views/Nav_View.vue
@@ -1,18 +1,33 @@
 <template>
-    <div class="top-bar" :class="darkMode ? '' : 'top-bar-dark'" />
+  <div class="top-bar" :class="darkMode ? 'top-bar-dark' : ''">
     <RouterLink class="logo" to="/">
-      <img :src="darkMode ? './logo_long.png' : './logo_long_dark.png'" alt="logo" class="logo" />
+      <img :src="darkMode ?  './logo_long_dark.png' : './logo_long.png'" alt="logo" class="logo" />
     </RouterLink>
     <div :class="['hamburger']">
       <div class="hamburger-box" @click="toggleSidebar()">
-        <span :class="['hamburger-line', 'top', { 'hamburger-line-dark': !darkMode }]"></span>
-        <span :class="['hamburger-line', 'middle', { 'hamburger-line-dark': !darkMode }]"></span>
-        <span :class="['hamburger-line', 'bottom', { 'hamburger-line-dark': !darkMode }]"></span>
+        <span :class="['hamburger-line', 'top', { 'hamburger-line-dark': darkMode }]"></span>
+        <span :class="['hamburger-line', 'middle', { 'hamburger-line-dark': darkMode }]"></span>
+        <span :class="['hamburger-line', 'bottom', { 'hamburger-line-dark': darkMode }]"></span>
       </div>
     </div>
     <Transition name="move">
-      <sidebar @theme="handleThemeChange" v-if="isSidebarOpen"/>
+      <sidebar :darkMode="darkMode" @theme="handleThemeChange" @bar="toggleSidebar" v-if="isSidebarOpen" />
     </Transition>
+    <div class="phone-bar" v-if="isPhone" :class="{'phone-bar-dark' : darkMode}" >
+      <RouterLink class="phone-bar-item" :class="{'phone-bar-item-dark': darkMode}" to="/">
+        <i :class="darkMode ? 'icon-home-dark' : 'icon-home'"></i>
+      </RouterLink>
+      <RouterLink class="phone-bar-item" :class="{'phone-bar-item-dark': darkMode}" to="/challenges">
+        <i :class="darkMode ? 'icon-challenges-dark' : 'icon-challenges'"></i>
+      </RouterLink>
+      <RouterLink class="phone-bar-item" :class="{'phone-bar-item-dark': darkMode}" to="/budget">
+        <i :class="darkMode ? 'icon-budget-dark' : 'icon-budget'"></i>
+      </RouterLink>
+    </div>
+  </div>
+  <Transition name="fade">
+    <div class="blur-screen" v-if="isSidebarOpen" @click="toggleSidebar" />
+  </Transition>
 </template>
 
 <script setup lang="ts">
@@ -20,7 +35,8 @@ import { onMounted, ref} from 'vue'
 import { Transition } from 'vue'
 import "@/assets/icons.css";
 import sidebar from '../components/nav/Sidebar.vue'
-const darkMode = ref(false)
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const darkMode = ref(prefersDarkMode);
 const isSidebarOpen = ref(false)
 const isPhone = ref(window.matchMedia('(max-width: 480px)').matches)
 
@@ -38,20 +54,24 @@ const toggleSidebar = () => {
   const top = document.querySelector('.top')
   const middle = document.querySelector('.middle')
   const bottom = document.querySelector('.bottom')
+  
+  const openClass = darkMode.value ? '-open-dark' : '-open'
+  const closeClass = darkMode.value ? '-close-dark' : '-close'
+  
   if (isSidebarOpen.value) {
-    top?.classList.remove('top-close')
-    top?.classList.add('top-open')
-    middle?.classList.remove('middle-close')
-    middle?.classList.add('middle-open')
-    bottom?.classList.remove('bottom-close')
-    bottom?.classList.add('bottom-open');
+    top?.classList.add(`top${openClass}`)
+    top?.classList.remove(`top${closeClass}`)
+    middle?.classList.add(`middle${openClass}`)
+    middle?.classList.remove(`middle${closeClass}`)
+    bottom?.classList.add(`bottom${openClass}`)
+    bottom?.classList.remove(`bottom${closeClass}`)
   } else {
-    top?.classList.remove('top-open')
-    top?.classList.add('top-close')
-    middle?.classList.remove('middle-open')
-    middle?.classList.add('middle-close')
-    bottom?.classList.remove('bottom-open')
-    bottom?.classList.add('bottom-close');
+    top?.classList.remove(`top${openClass}`)
+    top?.classList.add(`top${closeClass}`)
+    middle?.classList.remove(`middle${openClass}`)
+    middle?.classList.add(`middle${closeClass}`)
+    bottom?.classList.remove(`bottom${openClass}`)
+    bottom?.classList.add(`bottom${closeClass}`)
   }
 }
 
@@ -66,25 +86,37 @@ const handleThemeChange = () => {
 </script>
 
 <style scoped>
+.blur-screen {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 997;
+}
 .top-bar{
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  height: 120px;
-  background-color: #4b644a;
+  height: 90px;
+  background-color: #ecffda;
+  border-bottom: 1px solid #4b644a;
   z-index: 998;
+  overflow: hidden;
 }
 .top-bar-dark{
   background-color: #23244b;
+  border-bottom: 1px solid #757bfd;
 }
 .logo {
   cursor: pointer;
   z-index: 999;
-  position: fixed;
-  top: 20px;
-  height: 120px;
-  left: -80px;
+  position: relative;
+  top: 5px;
+  height: 100px;
+  left: -30px;
 }
 .hamburger {
   cursor: pointer;
@@ -94,19 +126,17 @@ const handleThemeChange = () => {
   justify-content: center;
   z-index: 1000;
   top: 0;
-  margin-top: 15px;
+  margin-top: 10px;
   right: 0;
-  margin-right: 10px;
-  height: 90px;
-  width: 90px;
+  margin-right: 12px;
+  height: 70px;
+  width: 80px;
   background-color: transparent;
   border-radius: 100px;
 }
-
 .hamburger-dark {
   background-color: #f0f0f0;
 }
-
 .hamburger-box {
   height: 60%;
   width: 65%;
@@ -115,7 +145,6 @@ const handleThemeChange = () => {
   align-items: center;
   justify-content: center;
 }
-
 .hamburger-line {
   position: absolute;
   border-radius: 20px;
@@ -126,7 +155,12 @@ const handleThemeChange = () => {
 .hamburger-line-dark {
   background-color: #757bfd;
 }
-
+.lightOpen {
+  background-color: #000000;
+}
+.darkOpen {
+  background-color: #f0f0f0;
+}
 .top {
   top: 0;
   z-index: 1000;
@@ -134,42 +168,77 @@ const handleThemeChange = () => {
 .top-open {
   animation: top-open 0.35s forwards;
 }
+.top-open-dark{
+  animation: top-open-dark 0.35s forwards;
+}
 .top-close {
   animation: top-close 0.35s forwards;
 }
-@keyframes top-open {
+.top-close-dark{
+  animation: top-close-dark 0.35s forwards;
+}
+@keyframes top-open-dark {
   0% {
     transform: translateY(0) rotate(0);
   }
   50% {
-    transform: translateY(24px) rotate(0);
+    transform: translateY(18px) rotate(0);
   }
   100% {
-    transform: translateY(24px) rotate(45deg);
+    transform: translateY(18px) rotate(45deg);
+  }
+}
+@keyframes top-open{
+  0% {
+    transform: translateY(0) rotate(0);
+  }
+  50% {
+    transform: translateY(18px) rotate(0);
+   
+  }
+  100% {
+    transform: translateY(18px) rotate(45deg);
     background-color: black;
   }
 }
 @keyframes top-close {
   0% {
-    transform: translateY(24px) rotate(45deg);
+    transform: translateY(18px) rotate(45deg);
     background-color: black;
   }
   50% {
-    transform: translateY(24px) rotate(0);
+    transform: translateY(18px) rotate(0);
+    background-color: black;
   }
   100% {
     transform: translateY(0) rotate(0);
   }
 }
-
+@keyframes top-close-dark {
+  0% {
+    transform: translateY(18px) rotate(45deg);
+  }
+  50% {
+    transform: translateY(18px) rotate(0);
+  }
+  100% {
+    transform: translateY(0) rotate(0);
+  }
+}
 .middle{
-  z-index: 999;
+  z-index: 998;
 }
 .middle-open {
   animation: middle-open 0.35s forwards;
 }
-.middle-close {
+.middle-open-dark{
+  animation: middle-open-dark 0.35s forwards;
+}
+.middle-close{
   animation: middle-close 0.35s forwards;
+}
+.middle-close-dark {
+  animation: middle-close-dark 0.35s forwards;
 }
 @keyframes middle-open {
   0% {
@@ -186,22 +255,48 @@ const handleThemeChange = () => {
     opacity: 0;
   }
 }
+@keyframes middle-open-dark {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 1;
+  }
+  70% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
+  }
+}
 @keyframes middle-close {
   0% {
     opacity: 0;
   }
+  40% {
+    opacity: 0;
+  }
   50% {
     opacity: 0;
-    background-color: black;
-  }
-  70% {
-    opacity: 1;
   }
   100% {
     opacity: 1;
   }
 }
-
+@keyframes middle-close-dark {
+  0% {
+    opacity: 0;
+  }
+  40% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 .bottom {
   z-index: 1000;
   bottom: 0;
@@ -209,28 +304,57 @@ const handleThemeChange = () => {
 .bottom-open {
   animation: bottom-open 0.35s forwards;
 }
+.bottom-open-dark {
+  animation: bottom-open-dark 0.35s forwards;
+}
 .bottom-close{
   animation: bottom-close 0.35s forwards;
+}
+.bottom-close-dark {
+  animation: bottom-close-dark 0.35s forwards;
+}
+@keyframes bottom-open-dark {
+  0% {
+    transform: translateY(0) rotate(0);
+  }
+  50% {
+    transform: translateY(-18px) rotate(0);;
+  }
+  100% {
+    transform: translateY(-18px) rotate(-45deg);
+  }
 }
 @keyframes bottom-open {
   0% {
     transform: translateY(0) rotate(0);
   }
   50% {
-    transform: translateY(-24px) rotate(0);;
+    transform: translateY(-18px) rotate(0);;
   }
   100% {
-    transform: translateY(-24px) rotate(-45deg);
+    transform: translateY(-18px) rotate(-45deg);
     background-color: black;
   }
 }
 @keyframes bottom-close {
   0% {
-    transform: translateY(-24px) rotate(-45deg);
+    transform: translateY(-18px) rotate(-45deg);
     background-color: black;
   }
   50% {
-    transform: translateY(-24px) rotate(0);
+    transform: translateY(-18px) rotate(0);
+    background-color: black;
+  }
+  100% {
+    transform: translateY(0) rotate(0);
+  }
+}
+@keyframes bottom-close-dark {
+  0% {
+    transform: translateY(-18px) rotate(-45deg);
+  }
+  50% {
+    transform: translateY(-18px) rotate(0);
   }
   100% {
     transform: translateY(0) rotate(0);
@@ -246,6 +370,52 @@ const handleThemeChange = () => {
 .move-enter-to, .move-leave-from {
   transform: translateX(0);
 }
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .35s ease-in-out;
+}
 
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-to, .fade-leave-from {
+  opacity: 1;
+}
+
+
+.phone-bar {
+  position: fixed;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 60px;
+  background-color: #ecffda;
+  border-top: 2px solid #4b644a;
+  z-index: 998;
+}
+.phone-bar-dark {
+  background-color: #23244b;
+  border-top: 2px solid #757bfd;
+}
+
+.phone-bar-item{
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+.phone-bar-item:hover{
+  background-color: #a6cd94;
+}
+.phone-bar-item-dark:hover{
+  background-color: #757bfd;
+}
 
 </style>let
