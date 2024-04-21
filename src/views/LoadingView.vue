3299f6b7931d49c37e7dd8e9bee46bae308becc8
Svein Kåre <sveiks@stud.ntnu.no>
2024-04-21 16:26:26 +0200

    Formatting

--- src/views/LoadingView.vue
+++ src/views/LoadingView.vue
@@ -5,35 +5,33 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import { getTokens } from '@/api/authenticationHooks'
+import { onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { getTokens } from "@/api/authenticationHooks";
 
 /**
  * When this component is mounted, it will extract the code parameter from the URL and use it to get an access token,
  * which it in turn uses to get the user's information.
  */
 onMounted(() => {
-  const route = useRoute()
-  const router = useRouter()
+  const route = useRoute();
+  const router = useRouter();
 
-  const code = route.query.code
-  if (!code || typeof code !== 'string') {
+  const code = route.query.code;
+  if (!code || typeof code !== "string") {
     // If there is no code parameter, redirect to home page
-    console.error('No code parameter found in URL')
-    router.push('/')
-    return
+    console.error("No code parameter found in URL");
+    router.push("/");
+    return;
   }
 
   getTokens(code)
-    .then(() => router.push('/'))
+    .then(() => router.push("/"))
     .catch(() => {
-      console.error('Failed to get tokens, returning to main menu')
-      router.push('/')
-    })
-})
+      console.error("Failed to get tokens, returning to main menu");
+      router.push("/");
+    });
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
