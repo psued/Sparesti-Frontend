fc30edbd7c497984c128358e0914e2c9cbc07ba7
anderebe <anderebe@stud.ntnu.no>
2024-04-22 16:45:49 +0200

    refactor: Created svg files to make a dynamic scalable road
    Made a component that houses the three new svg files that can be ligned up to create a scalable road. It is also dynamic with css styling. New Component: RoadTiles.vue
    
    Added three new css classes for the three new svg's in the icons.css

---
+++ src/components/road/RoadTiles.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="road-container">
+        <div class="road-box">
+            <svg class="road-svg road-right-light"></svg>
+            <svg class="road-svg road-left-light"></svg>
+            <svg class="road-svg road-right-light"></svg>
+            <svg class="road-svg road-left-light"></svg>
+            <svg class="road-svg road-right-light"></svg>
+            <svg class="road-svg road-left-light"></svg>
+            <svg class="road-svg road-right-light"></svg>
+            <svg class="road-svg road-left-light"></svg>
+            <svg class="road-svg road-right-light"></svg>
+            <svg class="road-svg road-left-light"></svg>
+            <svg class="road-svg road-right-light"></svg>
+            <svg class="road-svg road-left-light"></svg>
+            <svg class="road-svg road-right-light"></svg>
+            <svg class="road-svg road-left-light"></svg>
+            <svg class="road-svg road-right-light"></svg>
+            <svg class="road-svg road-left-light"></svg>
+            <svg class="road-svg road-right-light"></svg>
+            <svg class="road-svg road-start-light"></svg>
+        </div>
+        </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.road-container{
+    flex-direction: column;
+    height: 86vh;
+    margin: 90px 0 0 0;
+    width: 100%;
+    position: fixed;
+    justify-content: center;
+    align-items: center;
+    overflow-y: auto;
+}
+.road-box {
+    position: fixed;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+    position: absolute;
+    width: 100vw;
+    overflow-x: hidden;
+    overflow-y: scroll; /* Use "scroll" to always show scrollbar */
+}
+
+.road-svg {
+    display: flex;
+    justify-content: center;
+    height: 160px;
+    width: 100%;
+    resize: none;
+}
+
+@media screen and (min-width: 481px){
+    .road-container{
+        height: 93vh;
+    }
+}
+</style>
