ff4aca02d58ca576b0249a12f9992690d6ccabe3
Scott Sanya Emonanekkul <scottse@stud.ntnu.no>
2024-04-22 14:43:46 +0200

    Removing uneccesary files, and updating path to FrontPageView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import AuthenticationView from '@/views/AuthenticationView.vue'
 import LoginForm from '@/components/forms/LoginForm.vue'
 import SignUpForm from '@/components/forms/SignUpForm.vue'
@@ -11,21 +10,8 @@ const router = createRouter({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/frontpage",
       name: "frontpage",
       component: FrontPageView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     },
     {
       path: '/authentication',
