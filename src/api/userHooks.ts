ff41bfa1dc675242ee49cffa68c33f3a1ea7b579
Zahid André Kristiansen <zahidak@stud.ntnu.no>
2024-04-22 13:06:02 +0200

    feat: :art: Added hooks for user
    Made userhooks to be able to fetch data later

--- src/api/userHooks.ts
+++ src/api/userHooks.ts
@@ -1,32 +1,31 @@
 import { api } from "@/api/axiosConfig";
 
 export const getUserByUsername = async (username: string): Promise<any | null> => {
-		try {
-			const response = await api.get(`/users/${username}`,{
-				params: {username: username}
-		})
-    return response.data || null
-  } catch (error) {
-    console.error('Error getting user by username:', error)
-    return null
-  }
-}
-
-export const updateProfilePicture = async (
-  username: string,
-  profilePictureUrl: string
-): Promise<boolean> => {
   try {
-    const response = await api.put(
-      `/user/profile-picture?username=${username}&profilePictureUrl=${profilePictureUrl}`
-    )
-
-    if (response.status === 200) {
-      return true
-    } else {
-      throw new Error(`Failed to update profile picture. Status: ${response.status}`)
-    }
+    const response = await api.get(`/api/users/${username}`);
+    return response.data || null;
   } catch (error) {
-    throw new Error(`Error updating profile picture: ${error}`)
+    console.error('Error getting user by username:', error);
+    return null;
   }
-}
+};
+
+export const getSavingsGoalsByUserId = async (userId: number): Promise<any[] | null> => {
+  try {
+    const response = await api.get(`/api/users/${userId}/savings-goals`);
+    return response.data; 
+  } catch (error) {
+    console.error('Error getting savings goals by user ID:', error);
+    return null; 
+  }
+};
+
+export const getChallenges = async (userId: number): Promise<any[] | null> => {
+  try {
+    const response = await api.get(`/api/users/${userId}/savings-goals`);
+    return response.data; 
+  } catch (error) {
+    console.error('Error getting savings goals by user ID:', error);
+    return null; 
+  }
+};
