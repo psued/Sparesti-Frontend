5d0fd51218380e33c57ca7ad3545d34c63bb44c0
sondreao <sondreao@stud.ntnu.no>
2024-04-29 14:40:59 +0200

    Addition: Back Button
    Added a reusable component that serves as a button if a user wishes to go back.

---
+++ src/components/assets/BackButtonComponent.vue
@@ -0,0 +1,36 @@
+<<template>
+  <div class="buttonContainer">
+    <div class="bgCircle">
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16 12H8M8 12L11 9M8 12L11 15" stroke="#729960" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="#729960" stroke-width="1.5" stroke-linecap="round"/>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['close'])
+
+const close = () => {
+  emit('close')
+}
+</script>
+
+<style scoped>
+.buttonContainer {
+  position: relative;
+  cursor: pointer;
+}
+
+.bgCircle {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #A6CD94;
+  border-radius: 30px;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
