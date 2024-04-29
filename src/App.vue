4f34a7937316bfe3b0e085badd555bbd8db4fa57
anderebe <anderebe@stud.ntnu.no>
2024-04-29 14:51:31 +0200

    refactor: Made the phone bar a seperate component and added it to App
    PhoneBar is now a distinct component, Footer_View.vue, and works correctly
    
    Need to add margin or padding to fix overlay issues with the phone bar

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,15 @@
       <RouterView/>
     </div>
     <FOOTER v-if="isPhone" class="footer-box"/>
-    
+
   </div>
 </template>
 
 <script setup lang="ts">
 import { useDark, useToggle } from "@vueuse/core";
 import { RouterLink, RouterView } from "vue-router";
-import { onMounted, ref } from "vue";
 import NAV from "./views/Nav_View.vue";
+import { onMounted, ref } from "vue";
 import FOOTER from "./views/Footer_View.vue";
 
 const isDark = useDark();
@@ -52,11 +52,13 @@ onMounted(() => {
   width: 100%;
   z-index: 1000;
 }
-#content{
+
+#content {
   position: relative;
   top: 90px;
   overflow-y: hidden;
 }
+
 @media screen and (max-width: 480px) {
   .content{
     bottom: 90px
