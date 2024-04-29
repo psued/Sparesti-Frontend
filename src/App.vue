5b56ea736cc2e17782d8f631472bf742702adcb5
anderebe <anderebe@stud.ntnu.no>
2024-04-29 15:40:42 +0200

    fix: Made it unable to zoom
    Zooming messed up some stuff. Is a small line in app. Easy to change

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <div class="layout">
     <div id="nav">
       <NAV />
@@ -69,7 +70,7 @@ onMounted(() => {
 
 @media screen and (max-width: 480px) {
   #content{
-    padding-bottom: 90px
+    padding-bottom: 60px
   }
 }
 </style>
