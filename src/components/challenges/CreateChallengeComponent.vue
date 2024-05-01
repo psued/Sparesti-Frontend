44404e3fe9b5f78e6fae6e5b4570b61106636220
sondreao <sondreao@stud.ntnu.no>
2024-05-01 08:37:21 +0200

    Addition: Radiobuttons Component
    Created a reusable radiobutton component that can be used by passing an array of strings as a prop to the component, and listening for the 'clickRadio' event.

--- src/components/challenges/CreateChallengeComponent.vue
+++ src/components/challenges/CreateChallengeComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="createChallengeContainer">
     <p>Challenge Title</p>
-    <input type="text" placeholder="Title"/>
+    <input type="text" placeholder="Title" id="challengeTitle"/>
 
     <p>Challenge Description</p>
     <textarea maxlength="60" id="challengeDescriptionInput" rows="3"></textarea>
@@ -11,58 +11,26 @@
 
     <p>Velg et tidsintervall</p>
 
-    <div id="timeIntervalRadio">
-      <ButtonComponent @click="clickDaily" :pressed="daily" class="timeIntervalButton">
-        <template v-slot:content>
-          <p class="timeIntervalText">Daglig</p>
-        </template>
-      </ButtonComponent>
-      <ButtonComponent @click="clickWeekly" :pressed="weekly" class="timeIntervalButton">
-        <template v-slot:content>
-          <p class="timeIntervalText">Ukentlig</p>
-        </template>
-      </ButtonComponent>
-      <ButtonComponent @click="clickMonthly" :pressed="monthly" class="timeIntervalButton">
-        <template v-slot:content>
-          <p class="timeIntervalText">Månedlig</p>
-        </template>
-      </ButtonComponent>
-    </div>
+    <RadioButtonsComponent @radioClick="pickTimeInterval" :radioButtons="['Daily', 'Weekly', 'Monthly']"/>
 
     <p>Hva slags type utfordring vil du gjøre?</p>
 
-    <div id="timeIntervalRadio">
-      <ButtonComponent @click="clickSave" :pressed="save" class="timeIntervalButton">
-        <template v-slot:content>
-          <p class="timeIntervalText">Spar</p>
-        </template>
-      </ButtonComponent>
-      <ButtonComponent @click="clickBuy" :pressed="buy" class="timeIntervalButton">
-        <template v-slot:content>
-          <p class="timeIntervalText">Kjøp</p>
-        </template>
-      </ButtonComponent>
-      <ButtonComponent @click="clickConsumption" :pressed="consumption" class="timeIntervalButton">
-        <template v-slot:content>
-          <p class="timeIntervalText">Forbruk</p>
-        </template>
-      </ButtonComponent>
-    </div>
+    <RadioButtonsComponent @radioClick="setChallengeType" :radioButtons="['Save', 'Buy', 'Consumption']"/>
 
-    <div v-if="challengeType === 'save'">
+    <div v-if="challengeType === 'Save'">
       <p>Sett deg et sparemål</p>
       <input type="number" min="0" step="1" placeholder="kr"/>
 
     </div>
 
-    <div v-if="challengeType === 'buy'">
+    <div v-if="challengeType === 'Buy'">
       <p>Produktnavn</p>
       <input type="text" placeholder="Produkt"/>
       <p>Kjøpsgrense</p>
       <input type="number" min="0" step="1" placeholder="Antall"/>
     </div>
 
-    <div v-if="challengeType === 'consumption'">
+    <div v-if="challengeType === 'Consumption'">
       <p>Sett deg et sparemål</p>
       <input type="number" min="0" step="1" placeholder="Title"/>
     </div>
@@ -76,6 +44,7 @@ import SVGButtonComponent from "@/components/assets/SVGButtonComponent.vue";
 import ButtonComponent from "@/components/assets/ButtonComponent.vue";
 import { ref } from "vue";
 import EmojiPickerComponent from "@/components/assets/EmojiPickerComponent.vue";
+import RadioButtonsComponent from "@/components/assets/RadioButtonsComponent.vue";
 
 const emoji = ref("");
 
@@ -84,53 +53,24 @@ function pickEmoji(e) {
   console.log(emoji.value);
 }
 
-const daily = ref(false);
-const weekly = ref(false);
-const monthly = ref(false);
 
-function clickDaily() {
-  daily.value = true;
-  weekly.value = false;
-  monthly.value = false;
-}
-function clickWeekly() {
-  daily.value = false;
-  weekly.value = true;
-  monthly.value = false;
-}
-function clickMonthly() {
-  daily.value = false;
-  weekly.value = false;
-  monthly.value = true;
+const timeInterval = ref("");
+function pickTimeInterval(interval) {
+  timeInterval.value = interval;
+  console.log(timeInterval.value);
 }
 
 const challengeType = ref("");
 
-const save = ref(false);
-const buy = ref(false);
-const consumption = ref(false);
-
-function clickSave() {
-  save.value = true;
-  buy.value = false;
-  consumption.value = false;
-  challengeType.value = "save";
-}
-function clickBuy() {
-  save.value = false;
-  buy.value = true;
-  consumption.value = false;
-  challengeType.value = "buy";
-}
-function clickConsumption() {
-  save.value = false;
-  buy.value = false;
-  consumption.value = true;
-  challengeType.value = "consumption";
+function setChallengeType(type) {
+  challengeType.value = type;
 }
 </script>
 
 <style scoped>
+#challengeTitle {
+
+}
 
 #createChallengeContainer {
   margin: 0 20px;
@@ -141,25 +81,5 @@ function clickConsumption() {
   text-align: center;
   resize: none;
   padding: 0 5px;
-}
-
-#timeIntervalRadio {
-  margin: 10px 0;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-
-}
-
-.timeIntervalButton {
-  width: 100px;
-  height: 30px;
-  display: inline-block;
-}
-
-.timeIntervalText {
-  font-size: 1rem;
-  font-weight: bold;
-  color: white;
 }
 </style>
