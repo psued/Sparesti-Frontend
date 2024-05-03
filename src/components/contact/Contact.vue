4de72a4d28d193bc0912b02043beca7d1c1afbd9
anderebe <anderebe@stud.ntnu.no>
2024-05-03 14:08:40 +0200

    quick commit

---
+++ src/components/contact/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="contact">
+        <text class="mail">Mail: falstfirm@mail.com</text>
+        <text class="phone">Tlf: +47 12 34 56 78</text>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.contact{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
